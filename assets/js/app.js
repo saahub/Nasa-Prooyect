ed4ca233e54dd470cf05cbee47c622779493baf0
saahub <carolina-saavedra1@hotmail.com>
2018-02-22 14:24:47 -0300

    subiendo

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,7 +60,7 @@ function showSecondInfo(info) {
   console.log(info.url)
   $('#contenedorVacio').empty()
   //$('#contenedorVacio').append(`<img src="${info.url}"`);
-  $('#contenedorVacio').append(`<img src="${info.url} alt=""><div class="responsive-img"</div>`);
+  $('#contenedorVacio').append(`<img src="${info.url} class="responsive-img" alt="">`);
 }
 
 // boton de busqueda
@@ -68,41 +68,46 @@ $('#search').click(function() {
  const inputValue = $('#input-search').val();
  const inputValueLower = inputValue.toLowerCase();
  $.ajax({
-   url : `https://images-api.nasa.gov/search?q=${inputValueLower}`,
-   type: 'GET',
-   datatype: 'json',
-   success: function(result) {
-     console.log(result.collection.items);
-     $.ajax({
-     url: result.collection.items[0].href,
-     type: 'GET',
-     datatype: 'json',
-     success: function(result) {
-       /*
-       * For each recibe cada elemento
-       * Las condiciones del if permiten que al recibir un video quede "apendeado" en un iframe
-       * En caso de ser una imagen se apendea en una etiqueta img.
-       */
-       result.forEach(el => {
+  url : `https://images-api.nasa.gov/search?q=${inputValueLower}`,
+  type: 'GET',
+  datatype: 'json',
+  success: function(result) {
+    console.log(result.collection.items);
+    $.ajax({
+    url: result.collection.items[0].href,
+    type: 'GET',
+    datatype: 'json',
+    success: function(result) {
+      /*
+      * For each recibe cada elemento
+      * Las condiciones del if permiten que al recibir un video quede "apendeado" en un iframe
+      * En caso de ser una imagen se apendea en una etiqueta img.
+      */
+      result.forEach(el => {
         /*
-           $('#contenedorVacio').append(`
-           <div class="col s12 xl12 col l12 col m12">
-           <div class="video-container">
-           
-           </div>
-           </div>`);
-           */
+          $('#contenedorVacio').append(`
+          div class="col s12 xl12 col l12 col m12">
+          <div class="video-container">
+          
+          </div>
+          </div>`);
+          */
           console.log(el)
-           $('#image-container').append(`<div class="col s12 xl12 col l12 col m12">
-            <iframe class="responsive-video" controls autoplay="false" src="${el.url}" type="text/html" frameborder="0"></iframe>
-            <img src="${el}" alt="" class="responsive-img"></div>`);
-       });
-     }
-   })
-   $('#image-container').empty();
-   }
+            $('#image-container').append(`<div class="col s12 xl12 col l12 col m12">
+              <div class="video-container">
+            <iframe class="responsive-video" controls autoplay="false" width="340" height="385" src="${el}" type="text/html" frameborder="0"></iframe>
+            </div> </div>`);
+      });
+    }
+  })
+  $('#image-container').empty();
+  }
  })
 });
 
-
-
+/*$('#image-container').append(`
+            <div class="col s12 xl12 col l12 col m12">
+            <div class="video-container">
+            <iframe class="responsive-video" controls autoplay="false"  src="${el}" type="text/html" frameborder="0"></iframe>
+            </div>
+            </div>`);*/
